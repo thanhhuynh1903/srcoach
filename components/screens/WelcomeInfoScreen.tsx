dc51b57980dab5bf8a07533eed0b19433aed952e
WickyPlays <nguyenthienbao8601@gmail.com>
2025-03-24 13:41:05 +0700

    Update welcome images

--- components/screens/WelcomeInfoScreen.tsx
+++ components/screens/WelcomeInfoScreen.tsx
@@ -5,26 +5,36 @@ import {
   StyleSheet,
   TouchableOpacity,
   StatusBar,
+  Image,
 } from 'react-native';
 import * as Progress from 'react-native-progress';
 import Icon from '@react-native-vector-icons/ionicons';
 import { wp } from '../helpers/common';
+import welcome_1 from '../assets/welcome_1.png';
+import welcome_2 from '../assets/welcome_2.png';
+import welcome_3 from '../assets/welcome_3.png';
+import welcome_4 from '../assets/welcome_4.png';
+
 const slides = [
   {
     heading: 'Helpful Resources & Community.',
     subheading: 'Join a community of 5,000+ users dedicating to healthy life with AI/ML.',
+    image: welcome_1,
   },
   {
     heading: 'Intuitive Nutrition & Med Tracker with AI',
     subheading: 'Easily track your medication & nutrition with the power of AI.',
+    image: welcome_2,
   },
   {
     heading: 'Emphatic AI Wellness Chatbot For All.',
     subheading: 'Experience compassionate and personalized care with our AI chatbot.',
+    image: welcome_3,
   },
   {
     heading: 'Your Intelligent Fitness Companion.',
     subheading: 'Track your calorie & fitness nutrition with AI and get special recommendations.',
+    image: welcome_4,
   },
 ];
 
@@ -64,7 +74,9 @@ const WelcomeInfoScreen = ({ navigation }: { navigation: any }) => {
         <Text style={styles.heading}>{slides[currentSlide].heading}</Text>
         <Text style={styles.subheading}>{slides[currentSlide].subheading}</Text>
       </View>
-      <View style={styles.illustrationContainer}>{/* Illustration goes here */}</View>
+      <View style={styles.illustrationContainer}>
+        <Image source={slides[currentSlide].image} style={styles.image} />
+      </View>
       {currentSlide > 0 && (
         <TouchableOpacity style={styles.fabLeft} onPress={handleBack}>
           <Icon size={30} color={'white'} name="arrow-back-outline" />
@@ -113,6 +125,14 @@ const styles = StyleSheet.create({
   illustrationContainer: {
     alignItems: 'center',
     marginTop: 40,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  image: {
+    width: wp(100),
+    height: wp(140),
+    transform: [{ translateY: 50 }],
+    resizeMode: 'cover',
   },
   fab: {
     position: 'absolute',
